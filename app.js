ce6b556aaa17af7702a3364b02b6dbbed3e743e4
Eylon <eylon.perets@gmail.com>
2022-01-13 18:05:37 +0200

    basic setup

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const path = require('path')
+
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+
+app.listen(3000, () => {
+    console.log('Serving on port 3000!')
+})
+
