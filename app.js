6df573bcdbcd9467ab2783423ad4bfb16b532385
Eylon <eylon.perets@gmail.com>
2022-01-23 21:18:13 +0200

    joi and more error handling

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
 const ejsMate = require('ejs-mate');
+const Joi = require('joi');
 const catchAsync = require('./utils/catchAsync');
+const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
 const Campground = require('./models/campgrounds');
 
@@ -33,6 +35,21 @@ app.get('/campgrounds/new', (req, res) => {
 });
 
 app.post('/campgrounds', catchAsync(async (req, res, next) => {
+    // if (!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
+    const campgroundSchema = Joi.object({
+        campground: Joi.object({
+            title: Joi.string().required(),
+            price: Joi.number().required().min(0),
+            image: Joi.string().required(),
+            location: Joi.string().required(),
+            description: Joi.string().required()
+        }).required()
+    })
+    const { error } = campgroundSchema.validate(req.body);
+    if (error) {
+        const msg = error.details.map(el => el.message).join(',');
+        throw new ExpressError(result.error.details, 400);
+    }
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`);
@@ -59,8 +76,14 @@ app.delete('/campgrounds/:id', catchAsync(async (req, res) => {
     res.redirect('/campgrounds');
 }));
 
+app.all('*', (req, res, next) => { // moves to app.use a new error (passed to next)
+    next(new ExpressError('Page Not found', 404));
+})
+
 app.use((err, req, res, next) => { //basic eerror handler
-    res.send('oh no')
+    const { statusCode = 500 } = err;
+    if (!err.message) err.message = 'oh no something went wrong!';
+    res.status(statusCode).render('error', { err });
 })
 
 
