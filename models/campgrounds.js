5dbb420cd4ca5493e7cc9c6e80ae4dbcb0d25cfd
Eylon <eylon.perets@gmail.com>
2022-02-02 00:03:40 +0200

    post func in schema

--- models/campgrounds.js
+++ models/campgrounds.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
+const { campgroundSchema } = require('../schemas');
 const Schema = mongoose.Schema;
+const Review = require('./review');
 
 const CampgroundSchema = new Schema({
     title: String,
@@ -14,6 +16,16 @@ const CampgroundSchema = new Schema({
         }
     ]
 });
+
+CampgroundSchema.post('findOneAndDelete', async function (doc) {
+     if(doc){
+        await Review.deleteMany({
+            _id: {
+                $in: doc.reviews
+            }
+        })
+     }
+})
 
 module.exports = mongoose.model('Campground', CampgroundSchema);
 
