52637cd0a691d34ad624e31941cf6efec710b9f4
Eylon <eylon.perets@gmail.com>
2022-03-14 14:00:11 +0200

    helmet + map location

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ const LocalStrategy = require('passport-local');
 const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
 const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
 
 const Campground = require('./models/campgrounds');
 const Review = require('./models/review');
@@ -40,6 +41,59 @@ app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(mongoSanitize());
 
+//helmet
+//CSP = allowing to load stuff for the document thats being served only from ->
+const scriptSrcUrls = [
+  'https://stackpath.bootstrapcdn.com/',
+  'https://api.tiles.mapbox.com/',
+  'https://api.mapbox.com/',
+  'https://kit.fontawesome.com/',
+  'https://cdnjs.cloudflare.com/',
+  'https://cdn.jsdelivr.net/',
+  'https://res.cloudinary.com/dv5vm4sqh/',
+];
+const styleSrcUrls = [
+  'https://kit-free.fontawesome.com/',
+  'https://stackpath.bootstrapcdn.com/',
+  'https://api.mapbox.com/',
+  'https://api.tiles.mapbox.com/',
+  'https://fonts.googleapis.com/',
+  'https://use.fontawesome.com/',
+  'https://cdn.jsdelivr.net/',
+  'https://res.cloudinary.com/dv5vm4sqh/',
+];
+const connectSrcUrls = [
+  'https://*.tiles.mapbox.com',
+  'https://api.mapbox.com',
+  'https://events.mapbox.com',
+  'https://res.cloudinary.com/dv5vm4sqh/',
+];
+const fontSrcUrls = ['https://res.cloudinary.com/dv5vm4sqh/'];
+
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: [],
+      connectSrc: ["'self'", ...connectSrcUrls],
+      scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+      styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+      workerSrc: ["'self'", 'blob:'],
+      objectSrc: [],
+      imgSrc: [
+        "'self'",
+        'blob:',
+        'data:',
+        'https://res.cloudinary.com/dp01hn09x/', //SHOULD MATCH YOUR CLOUDINARY ACCOUNT!
+        'https://images.unsplash.com/',
+      ],
+      fontSrc: ["'self'", ...fontSrcUrls],
+      mediaSrc: ['https://res.cloudinary.com/dv5vm4sqh/'],
+      childSrc: ['blob:'],
+    },
+  })
+);
+
+//
 const sessionConfig = {
   name: 'session', //so it doesnt show up as 'connection.sid' and is easily targeted, not hidden but not default
   secret: 'thisshouldbeabettersecret!',
